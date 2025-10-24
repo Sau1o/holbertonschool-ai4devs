51c5ac74b164ef7094263ea0f8c3b16a60511a69
Saulo Coelho <coelho.saulo@gmail.com>
2025-10-24 18:35:35 -0300

    Create bug5.py

---
+++ prompt_engineering/bug_snippets/bug5.py
@@ -0,0 +1,8 @@
+# Intended to sort a list of numbers in ascending order
+
+import math
+
+numbers = [10, 5, 20, 1]
+sorted_numbers = math.sort(numbers)  # math module doesn't have 'sort'
+
+print(sorted_numbers)
