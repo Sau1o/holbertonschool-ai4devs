5cc021ff2f7676f594b0436a5da20023564c4084
Saulo Coelho <coelho.saulo@gmail.com>
2025-11-10 06:50:40 -0300

    Create bug3.java

---
+++ bug_snippets/bug3.java
@@ -0,0 +1,7 @@
+// Intended: Print the 3rd element of the array
+public class Bug3 {
+  public static void main(String[] args) {
+    int[] nums = {10, 20};
+    System.out.println(nums[2]);
+  }
+}
