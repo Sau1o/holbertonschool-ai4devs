e26a4b8843fea43fad883edd01d737a22b0e76b8
Saulo Coelho <coelho.saulo@gmail.com>
2025-12-03 06:56:55 -0300

    Update bug2.js

--- prompting_debug_assistant/bug_snippets/bug2.js
+++ prompting_debug_assistant/bug_snippets/bug2.js
@@ -1,10 +1,15 @@
-// Intended: Check if a number is even
-function isEven(num) {
-  if (num % 2 == 1) {
-    return true; // wrong condition
-  } else {
-    return false;
-  }
+// Logical error example: intended to compute the nth Fibonacci number (0-indexed)
+function fib(n) {
+    // Intended: fib(0)=0, fib(1)=1, fib(2)=1, ...
+    if (n <= 1) return n;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < n; i++) {
+        // BUG: swapped update order causes incorrect sequence
+        a = b;
+        b = a + b;
+    }
+    return b;
 }
 
-console.log(isEven(4)); // should be true
+console.log(fib(6)); // expected 8 but will print wrong value
