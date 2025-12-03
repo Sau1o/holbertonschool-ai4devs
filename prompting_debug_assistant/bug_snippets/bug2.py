0c3f2c27e809cb34cc6e7293459af6650bd6dd85
Saulo Coelho <coelho.saulo@gmail.com>
2025-12-03 06:26:44 -0300

    Create bug2.py

---
+++ prompting_debug_assistant/bug_snippets/bug2.py
@@ -0,0 +1,10 @@
+// Intended: Check if a number is even
+function isEven(num) {
+  if (num % 2 == 1) {
+    return true; // wrong condition
+  } else {
+    return false;
+  }
+}
+
+console.log(isEven(4)); // should be true
